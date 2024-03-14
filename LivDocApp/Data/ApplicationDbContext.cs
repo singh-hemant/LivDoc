c8be928a61ce143d4753884098cd577c59c2e7fe
AzureVMHemant <AzureVMHemant@az204-vm1>
2024-03-14 09:53:43 +0000

    Add project files.

---
+++ LivDocApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using LivDocApp.Data;
+using LivDocApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LivDocApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Employee>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new EmployeeEntityConfiguration());
+        }
+    }
+}
+
+internal class EmployeeEntityConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.Property(u=>u.Name).HasMaxLength(255);
+        builder.Property(u=>u.SapId).HasMaxLength(8);
+        builder.Property(u=>u.Location).HasMaxLength(255);
+    }
+}
+
---
+++ LivDocApp/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LivDocApp.Models
+{
+    public class Employee: IdentityUser
+    {
+
+    
+        public required string Name { get; set; }
+ 
+
+        public required string SapId { get; set; }
+     
+
+        public required string Location { get; set; }
+       
+
+    }
+}
