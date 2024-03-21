ec372ea250b9d83ca0fec2c9894b297897e26655
Hemant Singh <sin-hem@outlook.com>
2024-03-21 23:00:45 +0000

    Working with UI/UX

--- LivDocApp/Controllers/HomeController.cs
+++ LivDocApp/Controllers/HomeController.cs
@@ -20,6 +20,19 @@ namespace LivDocApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
 
         public IActionResult Search(string searchTerm)
