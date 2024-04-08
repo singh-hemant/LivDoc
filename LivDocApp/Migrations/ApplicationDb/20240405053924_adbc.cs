e27a55fcc9328f1cf85ec5f77cbb317a983b4666
SIN_HEM <sin-hem2@outlook.com>
2024-04-08 06:38:25 +0000

    v2.0.0

---
+++ LivDocApp/Migrations/ApplicationDb/20240405053924_adbc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LivDocApp.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class adbc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
